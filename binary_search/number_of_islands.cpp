0fd75370e4973cb0b55c8bbcde723d787d5a2a9b
IumoInfinium <iumoinfinium1331@gmail.com>
2022-10-01 17:50:35 +0530

    binarySearch daily-38

---
+++ binary_search/number_of_islands.cpp
@@ -0,0 +1,27 @@
+int m = -1, n = -1;
+void traverse(int i, int j, vector<vector<int>>& grid) {
+    if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0) return;
+
+    grid[i][j] = 0;
+    traverse(i, j + 1, grid);
+    traverse(i + 1, j, grid);
+    traverse(i, j - 1, grid);
+    traverse(i - 1, j, grid);
+    return;
+}
+
+int solve(vector<vector<int>>& grid) {
+    int m = grid.size(), n = grid[0].size();
+    int islands = 0;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j]) {
+                islands++;
+                traverse(i, j, grid);
+            }
+        }
+    }
+
+    return islands;
+}
