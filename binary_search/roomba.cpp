203479368987a0cb7b484e145b20888206662981
IumoInfinium <iumoinfinium1331@gmail.com>
2022-10-08 12:37:19 +0530

    binarySearch daily-45 v2.0

--- binary_search/roomba.cpp
+++ binary_search/roomba.cpp
@@ -1,10 +1,20 @@
 bool solve(vector<string>& moves, int x, int y) {
-    int currx=0,curry=0;
+    // int currx=0,curry=0;
+    // for(string dir:moves){
+    //     if(dir=="NORTH")     curry++;
+    //     else if(dir=="SOUTH")curry--;
+    //     else if(dir=="EAST") currx++;
+    //     else                 currx--;
+    // }
+    // return (currx==x && curry==y)?true: false;
+
     for(string dir:moves){
-        if(dir=="NORTH")     curry++;
-        else if(dir=="SOUTH")curry--;
-        else if(dir=="EAST") currx++;
-        else                 currx--;
+        if(dir=="NORTH")     y--;
+        else if(dir=="SOUTH")y++;
+        else if(dir=="EAST") x--;
+        else                 x++;
     }
-    return (currx==x && curry==y)?true: false;
+    return (!x&& !y)?true: false;
+
+
 }
