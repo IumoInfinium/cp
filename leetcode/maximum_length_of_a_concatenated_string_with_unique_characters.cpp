0d0a505b7b126b57330a62fec0743bba85b53bdc
IumoInfinium <iumoinfinium1331@gmail.com>
2022-10-24 16:08:15 +0530

    lc-d-86 v2.0

--- leetcode/maximum_length_of_a_concatenated_string_with_unique_characters.cpp
+++ leetcode/maximum_length_of_a_concatenated_string_with_unique_characters.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-
+/*
     //<============= METHOD 1==================>
     int maxLength(vector<string>& arr) {
         return dfs(arr,"",0);
@@ -14,6 +14,29 @@ public:
         int res = s.size();
         for(int i=idx; i <arr.size(); i++){
             res = max(res, dfs(arr, s+arr[i],i+1));
+        }
+        return res;
+    }
+*/
+    //<============= METHOD 2==================>
+    int maxLength(vector<string>& arr){
+        vector<bitset<26>> nbits;
+        for(string s : arr){
+            bitset<26> tmp;
+
+            // setting occurance of a char in bitset and push it to n-sized vector bitset
+            for(char c : s) tmp.set(c-'a');
+            if(tmp.count() == s.size()) nbits.push_back(tmp);
+
+        }
+        return dfs(nbits, bitset<26>(), 0);
+    }
+
+    int dfs(vector<bitset<26>>& nbits, bitset<26> curr, int idx){
+        int res = curr.count();
+        for(int i=idx; i < nbits.size(); i++){
+            if( !(nbits[i] & curr).any())
+                res = max(res, dfs(nbits, curr | nbits[i],i+1));
         }
         return res;
     }
