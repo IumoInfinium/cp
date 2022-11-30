a00fa047c18540aa0c1c052f7a2ff6476c23e450
IumoInfinium <iumoinfinium1331@gmail.com>
2022-11-30 10:39:26 +0530

    lc-u d-123 avg runtime & better mem

--- leetcode/unique_number_of_occurances.cpp
+++ leetcode/unique_number_of_occurances.cpp
@@ -10,17 +10,37 @@ public:
         // }
         // return true;
 
-        unordered_map<int,int> map;
-        int max_times=1;
-        for(int i : arr){
-            map[i]++;
-            if(map[i]>max_times) max_times = map[i];
-        }
-        vector<bool> counts(max_times,false);
-        for(auto& it : map){
-            if(counts[it.second]) return false;
-            counts[it.second]=true;
+        // better runtime than set
+        // unordered_map<int,int> map;
+        // int max_times=1;
+        // for(int i : arr){
+        //     map[i]++;
+        //     if(map[i]>max_times) max_times = map[i];
+        // }
+        // vector<bool> counts(max_times,false);
+        // for(auto& it : map){
+        //     if(counts[it.second]) return false;
+        //     counts[it.second]=true;
+        // }
+        // return true;
+
+        // better memory usage
+        int uniqs[1000]={0};
+        int count=0;
+        for(int i=0;i<arr.size(); i++){
+            count =0;
+            if(arr[i]==1001) continue;
+            
+            for(int j=i+1;j<arr.size(); j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                    arr[j]=1001;
+                }
+            }
+            if(uniqs[count]) return false;
+            uniqs[count]=1;
         }
         return true;
+
     }
 };
