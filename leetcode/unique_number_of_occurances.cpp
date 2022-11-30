b5fbd1acc6a89358f925a2b426a07ae426dd3575
IumoInfinium <iumoinfinium1331@gmail.com>
2022-11-30 10:26:23 +0530

    lc-u d-123 unique occurances better runtime

--- leetcode/unique_number_of_occurances.cpp
+++ leetcode/unique_number_of_occurances.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
+        // unordered_map<int,int> map;
+        // for(int i : arr) map[i]++;
+        // unordered_set<int> s;
+        // for(auto& it : map){
+        //     if(s.count(it.second)) return false;
+        //     s.insert(it.second);
+        // }
+        // return true;
+
         unordered_map<int,int> map;
-        for(int i : arr) map[i]++;
-        unordered_set<int> s;
+        int max_times=1;
+        for(int i : arr){
+            map[i]++;
+            if(map[i]>max_times) max_times = map[i];
+        }
+        vector<bool> counts(max_times,false);
         for(auto& it : map){
-            if(s.count(it.second)) return false;
-            s.insert(it.second);
+            if(counts[it.second]) return false;
+            counts[it.second]=true;
         }
         return true;
     }
