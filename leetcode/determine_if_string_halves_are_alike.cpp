13fa1959fb2724b147867c85cba17be04b2109ed
IumoInfinium <iumoinfinium1331@gmail.com>
2022-12-01 10:15:17 +0530

    lc-u d-124 better memory usage

--- leetcode/determine_if_string_halves_are_alike.cpp
+++ leetcode/determine_if_string_halves_are_alike.cpp
@@ -8,18 +8,29 @@ public:
             default: return false; break;
         }
     }
-    bool halvesAreAlike(string s) {
-        int n = s.size();
-        int c1=0,c2=0;
-        int low=0, high=n-1;
-        while(low<high){
-            if(charIs(s[low])) c1++;
-            if(charIs(s[high]))c2++;
-            low++;
-            high--;
-        }
+    // bool halvesAreAlike(string s) {
+    //     int n = s.size();
+    //     int c1=0,c2=0;
+    //     int low=0, high=n-1;
+    //     while(low<high){
+    //         if(charIs(s[low])) c1++;
+    //         if(charIs(s[high]))c2++;
+    //         low++;
+    //         high--;
+    //     }
 
-        if(c1 == c2) return true; 
-        return false;
+    //     if(c1 == c2) return true; 
+    //     return false;
+    // }
+
+    bool halvesAreAlike(string& s){
+        int c1=0,c2=0;
+        int n=s.size();
+        for(int i=0;i<n/2;i++){
+            if(charIs(s[i])) c1++;
+            if(charIs(s[n-i-1])) c2++;
+        }
+        if(c1 != c2) return false;
+        return true;
     }
 };
