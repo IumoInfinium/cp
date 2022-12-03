0dc6c2e2fd094e8e18205350581dd1e25f854897
IumoInfinium <iumoinfinium1331@gmail.com>
2022-12-03 17:42:39 +0530

    lc-u d-126 remove map

--- leetcode/sort_characater_by_frequency.cpp
+++ leetcode/sort_characater_by_frequency.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
-    string frequencySort(string& s) {
-        unordered_map<char,int> map;
+    // string frequencySort(string& s) {
+    //     unordered_map<char,int> map;
+    //     for(char& c : s){
+    //         map[c]++;
+    //     }
+    //     vector<pair<char,int>> freq_pair;
+    //     for(auto & it : map){
+    //         freq_pair.push_back({it.first,it.second});
+    //     }
+    //     sort(begin(freq_pair),end(freq_pair),[&](const pair<char,int>& a, const pair<char,int>& b){
+    //         return a.second > b.second; 
+    //     });
+    //     string ans;
+    //     for(auto& p : freq_pair){
+    //         int x = p.second;
+    //         while(x--){
+    //             ans += p.first;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    string frequencySort(string& s){
+        vector<pair<char,int>> freq(122,make_pair('a',0));
         for(char& c : s){
-            map[c]++;
+            freq[c-'0'] = {c,freq[c-'0'].second+1};
+            // cout << freq[c-'0'].second <<endl;
         }
-        vector<pair<char,int>> freq_pair;
-        for(auto & it : map){
-            freq_pair.push_back({it.first,it.second});
-        }
-        sort(begin(freq_pair),end(freq_pair),[&](const pair<char,int>& a, const pair<char,int>& b){
-            return a.second > b.second; 
-        });
         string ans;
-        for(auto& p : freq_pair){
-            int x = p.second;
+        sort(begin(freq),end(freq),[&](const pair<char,int>& a, const pair<char,int>& b){
+            return a.second > b.second;
+        });
+        int x=0;
+        for(auto&p : freq){
+            x = p.second;
+            if(!x) return ans;
             while(x--){
-                ans += p.first;
+                ans+=p.first;
             }
         }
         return ans;
