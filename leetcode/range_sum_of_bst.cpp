c4439b9a8e0b173027f450ddedb3eb9f24838fb4
IumoInfinium <iumoinfinium1331@gmail.com>
2022-12-07 17:33:21 +0530

    lc-u d-130 range sum in tree, better runtime and mem

--- leetcode/range_sum_of_bst.cpp
+++ leetcode/range_sum_of_bst.cpp
@@ -11,12 +11,28 @@
  */
 class Solution {
 public:
-    int sum=0;
-    int rangeSumBST(TreeNode* root, int low, int high) {
+
+    // // searchs complete binary tree
+    // int sum=0;
+    // int rangeSumBST(TreeNode* root, int low, int high) {
+    //     if(!root) return 0;
+    //     if(root->val >=low && root->val <=high )sum += root->val;
+    //     rangeSumBST(root->left,low,high);
+    //     rangeSumBST(root->right,low,high);
+    //     return sum;    
+    // }
+
+    // seaches only set of nodes in tree
+    int sum = 0;
+    int rangeSumBST(TreeNode *root, int low ,int high){
         if(!root) return 0;
-        if(root->val >=low && root->val <=high )sum += root->val;
-        rangeSumBST(root->left,low,high);
-        rangeSumBST(root->right,low,high);
-        return sum;    
+        if(root->val >= low && root->val <=high) sum += root->val;
+        if(root->left!= NULL && root->left->val < low) rangeSumBST(root->left->right,low,high);
+        else rangeSumBST(root->left,low,high);
+
+        if(root->right!= NULL && root->right->val > high) rangeSumBST(root->right->left,low,high);
+        else rangeSumBST(root->right,low,high);
+
+        return sum;
     }
 };
