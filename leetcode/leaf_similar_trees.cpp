67080b5389930b44bd0c3d5b31b89fdead622306
IumoInfinium <iumoinfinium1331@gmail.com>
2022-12-08 10:44:27 +0530

    lc-u d-131 similar leaves, slight diff

--- leetcode/leaf_similar_trees.cpp
+++ leetcode/leaf_similar_trees.cpp
@@ -11,18 +11,35 @@
  */
 class Solution {
 public:
-    vector <int> leaf1, leaf2;
+    // vector <int> leaf1, leaf2;
+
+    // bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+    //     traverse(root1,leaf1);
+    //     traverse(root2,leaf2);
+    //     return leaf1 == leaf2;
+    // }
+
+    // void traverse(TreeNode* root,vector<int>& leaves){
+    //     if(root==NULL) return ;
+    //     traverse(root->left, leaves);
+    //     if(root->left == NULL && root->right==NULL)
+    //         leaves.push_back(root->val);
+    //     traverse(root->right,leaves);        
+    // }
+    string leaf1, leaf2;
+
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
         traverse(root1,leaf1);
         traverse(root2,leaf2);
         return leaf1 == leaf2;
     }
 
-    void traverse(TreeNode* root,vector<int>& leaves){
-        if(root==NULL) return ;
-        traverse(root->left, leaves);
-        if(root->left == NULL && root->right==NULL)
-            leaves.push_back(root->val);
-        traverse(root->right,leaves);        
+    void traverse(TreeNode* root,string& leaves){
+        if(!root) return;
+        if(!root->left && !root->right) leaves += to_string(root->val)+',';
+        traverse(root->left,leaves);
+        traverse(root->right,leaves);
     }
+
+
 };
