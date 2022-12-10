0ce77ffd4c1790d6c3cff9a2c0f78f7896ff402d
IumoInfinium <iumoinfinium1331@gmail.com>
2022-12-10 18:40:19 +0530

    lc-u d-133 another method for max-product 2-BST

--- leetcode/maximum_product_of_splitted_binary_tree.cpp
+++ leetcode/maximum_product_of_splitted_binary_tree.cpp
@@ -11,18 +11,37 @@
  */
 class Solution {
 public:
-    long ans = 0, total = 0, s;
-    int maxProduct(TreeNode* root) {
-        if(!root) return 0;
-        total = summ(root);
-        summ(root);
-        return ans % (int)(1e9 + 7);
-    }
+    // long ans = 0, total = 0, s;
+    // int maxProduct(TreeNode* root) {
+    //     if(!root) return 0;
+    //     total = summ(root);
+    //     summ(root);
+    //     return ans % (int)(1e9 + 7);
+    // }
 
-    int summ(TreeNode* root){
-        if(!root) return 0;
-        s = root->val + summ(root->left) + summ(root->right);
-        ans = max(ans, s*(total-s));
-        return s;
+    // int summ(TreeNode* root){
+    //     if(!root) return 0;
+    //     s = root->val + summ(root->left) + summ(root->right);
+    //     ans = max(ans, s*(total-s));
+    //     return s;
+    // }
+
+    int findSum(TreeNode* node, vector<int>& sums){
+        if(!node) return 0;
+        auto lsum = findSum(node->left, sums);
+        auto rsum = findSum(node->right,sums);
+        auto summ = lsum + rsum + node->val;
+        sums.push_back(summ);
+        return summ;
+    }
+    int maxProduct(TreeNode* root){
+        int mod = 1e9+7;
+        vector<int> sums;
+        auto total = findSum(root,sums);
+        long maxProd = 0;
+        for( long val : sums)
+            maxProd = max(maxProd, val* (total-val));
+        return maxProd % mod;
     }
 };
+
