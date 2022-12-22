04d4ce96f78175fc5f56a193cf43ae27d5f758ed
IumoInfinium <iumoinfinium1331@gmail.com>
2022-12-22 16:03:07 +0530

    lc d-145 sum every ith node visits ~DP on trees

--- leetcode/sum_of_distances_in_tree.cpp
+++ leetcode/sum_of_distances_in_tree.cpp
@@ -1,29 +1,52 @@
 class Solution {
 public:
-    int dfs(int steps,int i, vector<vector<int>> adj, vector<bool>& vis){
-        if(vis[i]) return steps;
-        int tmp_sum=0;
-        vis[i] = true;
-        for(auto& x : adj[i]){
-            tmp_sum+=dfs(steps+1, x, adj, vis);
+    // Approach: PreOrder and PostOrder DFS
+    //   Intuition:
+    //    What if given a tree, with a certain root 0?
+    //    In O(N) we can find sum of distances in tree from root and all other nodes.
+    //    Now for all N nodes?
+    //  
+    //   When we move our root from one node to its connected node, one part of nodes get closer, one the other part get further.
+    //   If we know exactly hom many nodes in both parts, we can solve this problem.
+    //  
+    //   With one single traversal in tree, we should get enough information for it and don't need to do it again and again.
+    //   Let's solve it with node 0 as root.
+
+    // Time Complexity - O(n) -> dfs1
+    //                 - O(n) -> dfs2
+
+    // Above method is also called "re-rooting" and is common in dp-on trees!
+    // count array to store number of nodes in ith subtree 
+    vector<unordered_set<int>> tree;
+    vector<int> ans, count;
+    
+    void dfs1(int root, int pre){
+        for(int i : tree[root]){
+            if(i == pre) continue;
+            dfs1(i,root);
+            count[root] += count[i];
+            ans[root] += ans[i]+count[i];
         }
-        return tmp_sum;
     }
 
+    void dfs2(int root,int pre){
+        for(int i : tree[root]){
+            if(i == pre) continue;
+            ans[i] = ans[root] -count[i] + count.size() - count[i];
+            dfs2(i, root);
+        }
+    }
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-        if(n == 1) return vector<int>{0};
-        if(n == 2) return vector<int>{1,1}; 
-        vector<vector<int>> adj(n);
-        for(auto &e : edges){
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+        tree.resize(n);
+        ans.assign(n,0);
+        count.assign(n,1);
+
+        for(auto& e: edges){
+            tree[e[0]].insert(e[1]);
+            tree[e[1]].insert(e[0]);
         }
-        vector<int> ans(n);
-        for(int i=0;i<n;i++){
-            vector<bool> vis(n);
-            ans[i] = dfs(0,i,adj,vis);
-            ans[i] -= (n-1);
-        }
+        dfs1(0,-1);
+        dfs2(0,-1);
         return ans;
     }
 };
