206bc2c3d2272e5eda300979ec12e2d4bdae11e7
IumoInfinium <iumoinfinium1331@gmail.com>
2022-12-23 13:58:21 +0530

    lc-u d-146 loop removol O(n^2) -> O(n)

--- leetcode/best_time_to_buy_and_sell_stock_with_cooldown.cpp
+++ leetcode/best_time_to_buy_and_sell_stock_with_cooldown.cpp
@@ -18,21 +18,37 @@ public:
     // }
 
     // bottom to top
+    // int maxProfit(vector<int>& prices){
+    //     int n = prices.size();
+    //     vector<vector<int>> dp(n+2, vector<int>( 2, 0));
+
+    //     for(int idx = n-1; idx >= 0 ;idx--){
+    //         for(int buy =0; buy <= 1; buy++){
+    //             if(buy == 1){
+    //                 dp[idx][buy] = max( -prices[idx] + dp[idx+1][0],
+    //                         0 + dp[idx+1][1]);
+    //             }
+    //             else{
+    //                 dp[idx][buy] = max(prices[idx] + dp[idx+2][1],
+    //                     0 + dp[idx+1][0]);
+    //             }
+    //         }
+    //     }
+    //     return dp[0][1];
+    // }
+
+    // loop removal
     int maxProfit(vector<int>& prices){
         int n = prices.size();
         vector<vector<int>> dp(n+2, vector<int>( 2, 0));
 
         for(int idx = n-1; idx >= 0 ;idx--){
-            for(int buy =0; buy <= 1; buy++){
-                if(buy == 1){
-                    dp[idx][buy] = max( -prices[idx] + dp[idx+1][0],
-                            0 + dp[idx+1][1]);
-                }
-                else{
-                    dp[idx][buy] = max(prices[idx] + dp[idx+2][1],
-                        0 + dp[idx+1][0]);
-                }
-            }
+            // buying
+            dp[idx][1] = max( -prices[idx] + dp[idx+1][0],
+                    0 + dp[idx+1][1]);
+            // selling
+            dp[idx][0] = max(prices[idx] + dp[idx+2][1],
+                0 + dp[idx+1][0]);
         }
         return dp[0][1];
     }
