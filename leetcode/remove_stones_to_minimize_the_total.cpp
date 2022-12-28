fe83ddf4b694152f53a51fa54c746741cd07b48c
IumoInfinium <iumoinfinium1331@gmail.com>
2022-12-28 17:31:25 +0530

    lc-u d-151 minimium sum  T(n) -> O(n)

--- leetcode/remove_stones_to_minimize_the_total.cpp
+++ leetcode/remove_stones_to_minimize_the_total.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
-    int minStoneSum(vector<int>& piles, int k) {
-        priority_queue<int> pq;
-        for(int x : piles) pq.push(x);
-        int ans, x;
-        while(k--){
-            // here, ans is used like a temp variable
-            x= pq.top();
-            ans= x;
-            pq.pop();
-            pq.push(ans-(x/2));
+    // int minStoneSum(vector<int>& piles, int k) {
+    //     priority_queue<int> pq;
+    //     for(int x : piles) pq.push(x);
+    //     int ans, x;
+    //     while(k--){
+    //         // here, ans is used like a temp variable
+    //         x= pq.top();
+    //         ans= x;
+    //         pq.pop();
+    //         pq.push(ans-(x/2));
+    //     }
+    //     // actual ans here
+    //     ans=0;
+    //     while(!pq.empty()){
+    //         ans+=pq.top();
+    //         pq.pop();
+    //     }
+    //     return ans;
+    // }
+
+    int minStoneSum(vector<int>& piles, int k){
+        // assigning buckets to every element and then reducing backwards
+        int freq[10001]={0};
+        int maxi=-1;
+        for(int i : piles){
+            freq[i]++;
+            maxi = max(maxi,i);
         }
-        // actual ans here
-        ans=0;
-        while(!pq.empty()){
-            ans+=pq.top();
-            pq.pop();
+        for(int i= maxi; i>=1 && k>0; i--){
+            while(freq[i] != 0 && k>0){
+                freq[i]--;
+                freq[(i+1)/2]++;
+                k--;
+            }
         }
+        int ans=0;
+        for(int i=maxi; i>0; i--)
+            ans += freq[i]*i;
         return ans;
     }
 };
