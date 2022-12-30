ae8538ff327528c93489abbbd957a6fdba595cee
IumoInfinium <iumoinfinium1331@gmail.com>
2022-12-30 15:54:16 +0530

    lc-u d-153 DAG src to target bfs->dfs

--- leetcode/all_paths_from_source_to_target.cpp
+++ leetcode/all_paths_from_source_to_target.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> paths;  
-    void bfs(int i, vector<vector<int>>& graph,vector<int> way){
-        if(i == graph.size()-1){
+    // vector<vector<int>> paths;  
+    // void bfs(int i, vector<vector<int>>& graph,vector<int> way){
+    //     if(i == graph.size()-1){
+    //         way.push_back(i);
+    //         paths.push_back(way);
+    //     }
+    //     way.push_back(i);
+    //     for(int& x : graph[i]){
+    //         bfs(x,graph,way);
+    //     }
+    //     return;
+    // }
+    // vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+    //     vector<int> way;
+    //     bfs(0,graph,way);
+    //     return paths;
+    // }
+    vector<vector<int>> paths;
+    void dfs(int i, vector<vector<int>>& graph, vector<int>& way){
+        if(i==graph.size()-1){
             way.push_back(i);
             paths.push_back(way);
+            way.pop_back();
         }
         way.push_back(i);
         for(int& x : graph[i]){
-            bfs(x,graph,way);
+            dfs(x,graph,way);
         }
+        way.pop_back();
         return;
     }
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph){
+        // paths is the collection of all ways to move from 0 to n-1
         vector<int> way;
-        bfs(0,graph,way);
+        dfs(0,graph,way);
         return paths;
     }
 };
