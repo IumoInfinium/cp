5d421eb4e1cb95de5c1ee5e7dc5ba07fcaae1c9f
IumoInfinium <iumoinfinium1331@gmail.com>
2023-01-06 23:30:16 +0530

    lc-u d-160 rmved sort,used freq buckets

--- leetcode/maximum_ice_cream_bars.cpp
+++ leetcode/maximum_ice_cream_bars.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
+    // int maxIceCream(vector<int>& costs, int coins) {
+    //     sort(begin(costs),end(costs));
+    //     int i=0, n =costs.size();
+    //     int ans=0;
+    //     while(i<n && coins>0){
+    //         if(costs[i]<=coins) {ans++;coins -= costs[i];}
+    //         i++;
+    //     }
+    //     return ans;
+    // }
+    
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(begin(costs),end(costs));
-        int i=0, n =costs.size();
-        int ans=0;
-        while(i<n && coins>0){
-            if(costs[i]<=coins) {ans++;coins -= costs[i];}
-            i++;
+        int freq[100001]={0};
+        int maxi=INT_MIN;
+        for(int& i : costs){
+            freq[i]++;
+            if(i > maxi) maxi=i;
+        }
+        int i=0,ans=0;
+        while(coins>0 && i<=maxi){
+            if(freq[i]>0 && i <=coins){ans++; coins-=i;freq[i]--;}
+            else i++;
         }
         return ans;
     }
