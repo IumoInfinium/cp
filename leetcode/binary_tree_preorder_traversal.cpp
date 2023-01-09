9013ddb8e634b40a6b72a1df83af4b62f220b0ad
IumoInfinium <iumoinfinium1331@gmail.com>
2023-01-09 15:29:58 +0530

    lc-u d-163 binary-tree iterative preorder

--- leetcode/binary_tree_preorder_traversal.cpp
+++ leetcode/binary_tree_preorder_traversal.cpp
@@ -11,12 +11,29 @@
  */
 class Solution {
 public:
-    vector<int> res;
-    vector<int> preorderTraversal(TreeNode* root) {
-        if(!root) return res;
-        res.push_back(root->val);
-        preorderTraversal(root->left);
-        preorderTraversal(root->right);
+    // vector<int> res;
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     if(!root) return res;
+    //     res.push_back(root->val);
+    //     preorderTraversal(root->left);
+    //     preorderTraversal(root->right);
+    //     return res;
+    // }
+
+    vector<int> preorderTraversal(TreeNode* root){
+        stack<TreeNode*> st;
+        vector<int> res;
+        TreeNode *curr=root;
+        while(curr || !st.empty()){
+            while(curr){
+                res.push_back(curr->val);
+                st.push(curr);
+                curr = curr->left;
+            }
+            curr = st.top();
+            st.pop();
+            curr=curr->right;
+        }
         return res;
     }
 };
