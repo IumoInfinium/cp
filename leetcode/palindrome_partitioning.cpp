329c415d1f75e90e3347e780f7e3a776ad0edafb
IumoInfinium <iumoinfinium1331@gmail.com>
2023-01-22 13:19:24 +0530

    lc d-176 all possible palindrome partitions

---
+++ leetcode/palindrome_partitioning.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> path;
+        helper(0,s,path,ans);
+        return ans;
+    }
+
+    void helper(int idx, string s, vector<string>& path, vector<vector<string>>& ans){
+        if(idx == s.size()){
+            ans.push_back(path);
+            return;
+        }
+        
+        for(int i=idx; i<s.size(); i++){
+            if(isPalindrome(s,idx,i)){
+                path.push_back(s.substr(idx,i-idx+1));
+                helper(i+1,s,path,ans);
+                path.pop_back();
+            }
+        }
+        
+    }
+
+    bool isPalindrome(string& s,int start, int end){
+        while(start <= end)
+            if(s[start++] != s[end--]) return false;
+        return true;
+    }
+};
