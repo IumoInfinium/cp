e2043b5588f370e83ec58cab8e0e9e876a752f1a
IumoInfinium <iumoinfinium1331@gmail.com>
2023-01-28 14:19:31 +0530

    lc-u d-182 disjoint intervals with binary bounds  in oops

--- leetcode/data_stream_as_disjoint_intervals.cpp
+++ leetcode/data_stream_as_disjoint_intervals.cpp
@@ -1,33 +1,69 @@
 class SummaryRanges {
-private:
-    set<int> interval;
-public:
-    SummaryRanges() {
+// private:
+//     set<int> interval;
+// public:
+//     SummaryRanges() {
         
-    }
+//     }
     
-    void addNum(int value) {
-        interval.insert(value);
-    }
+//     void addNum(int value) {
+//         interval.insert(value);
+//     }
     
-    vector<vector<int>> getIntervals() {
-        vector<vector<int>> ans;
-        int start = -1, end = -1;
+//     vector<vector<int>> getIntervals() {
+//         vector<vector<int>> ans;
+//         int start = -1, end = -1;
 
-        for(const auto& item : interval){
-            if(start == -1){
-                start = item;
-                end = item;
-            }
-            else if(item == end + 1) end = item;
-            else{
-                ans.push_back({start,end});
-                start = end = item;
-            }
+//         for(const auto& item : interval){
+//             if(start == -1){
+//                 start = item;
+//                 end = item;
+//             }
+//             else if(item == end + 1) end = item;
+//             else{
+//                 ans.push_back({start,end});
+//                 start = end = item;
+//             }
+//         }
+//         if(start != -1) ans.push_back({start,end});
+//         return ans;
+//     }
+
+
+// Solution 2------------------------
+private:
+    map<int,int> intervals;
+public:
+    SummaryRanges() {}
+    
+    void addNum(int value){
+        int start = value, end = value;
+        auto moving_end = intervals.upper_bound(value);
+
+        if(moving_end != intervals.begin()){
+            auto moving_start = moving_end;
+            --moving_start; 
+
+            if(moving_start->second >= value) return;
+            if(moving_start->second == value-1) start = moving_start->first;
         }
-        if(start != -1) ans.push_back({start,end});
+        if(moving_end != intervals.end() && moving_end->first == value+1){
+            end = moving_end->second;
+            intervals.erase(moving_end);
+        }
+
+        intervals[start] = end;
+    }
+
+    vector<vector<int>> getIntervals(){
+        vector<vector<int>> ans;
+
+        for(const auto &item : intervals){
+            ans.push_back({item.first, item.second});
+        }
         return ans;
     }
+
 };
 
 /**
