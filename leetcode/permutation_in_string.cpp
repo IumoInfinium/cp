88aeee8284f9d40bb9f8d5379cc7752825190705
IumoInfinium <iumoinfinium1331@gmail.com>
2023-02-04 10:00:28 +0530

    lc-u d-189 optimized string permutation checking

--- leetcode/permutation_in_string.cpp
+++ leetcode/permutation_in_string.cpp
@@ -14,4 +14,26 @@ public:
         }
         return false;
     }
+    bool checkInclusion2(string s1, string s2) {
+        int n = s1.length(), m = s2.length();
+        if(n>m) return false;
+        
+        // mapping the strings
+        vector<int> map1(26,0), map2(26,0);
+        int slow=0, fast=0;
+        for(;fast<n; fast++){
+            map1[s1[fast]-'a']++;
+            map2[s2[fast]-'a']++;
+        }
+        if(map1 == map2) return true;
+        while(fast<m){
+            map2[s2[fast]-'a']++;
+            map2[s2[slow]-'a']--;
+            if(map1 == map2) return true;
+            slow++;
+            fast++;
+        }
+        return false;
+    }
+
 };
