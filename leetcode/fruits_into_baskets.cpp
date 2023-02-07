71ba73b3500af4efbe17edc3c735f557f4821148
IumoInfinium <iumoinfinium1331@gmail.com>
2023-02-07 16:59:33 +0530

    lc-u d-192 max count using dp

--- leetcode/fruits_into_baskets.cpp
+++ leetcode/fruits_into_baskets.cpp
@@ -24,50 +24,54 @@ public:
     //             }
     //             j++;
     //         }
-    //         ans = max(ans,count);
-    //     }
-    //     return ans;
-    // }
-
-    // WRONG ANSWER
-    // int totalFruit(vector<int>& fruits){
-    //     int n = fruits.size();
-    //     if(n==1) return 1;
-    //     if(n==2) return 2;
-
-    //     int b1 = fruits[0];
-    //     int b2 = fruits[1];
-    //     if(b1 == b2) b2 = -1;
-    //     int ans = 1, count = 2;
-    //     for(int i=2; i<n; i++){
-    //         if(fruits[i] == b1 || fruits[i] == b2) count++;
-    //         else if(b2== -1) {b2=fruits[i]; count++;}
-    //         else {
-    //             count=2;
-    //             b1 = fruits[i-1];
-    //             b2 = fruits[i];
-    //         }
-    //         ans = max(ans,count);
+    //         ans = max(ans,count)
     //     }
     //     return ans;
     // }
 
     int totalFruit(vector<int>& fruits){
-        unordered_map<int,int> map;
+        int n = fruits.size();
+        if(n==1) return 1;
+        if(n==2) return 2;
+        
+        int ans = 0;
+        
+        int b1 = -1, b2 = -1;
+        int curr = 0, countLast = 0;
 
-        int ans=1;
-        int n=fruits.size();
-        int i=0, j=0;
-        while(i<n){
-            map[fruits[i]]++;
-            if(map.size() <= 2) ans = max(ans, i-j+1);
-            else{
-                if(map[fruits[j]] == 1) map.erase(fruits[j]);
-                else map[fruits[j]]--;
-                j++;
+        for(int i=0; i<n; i++){
+            if(fruits[i] == b1 || fruits[i] == b2) curr++;
+            else curr = countLast+1;
+
+            if(fruits[i] == b1) countLast++;
+            else countLast =1;
+
+            if(fruits[i] != b1){
+                b2 = b1;
+                b1 = fruits[i];
             }
-            i++;
+            ans = max(ans, curr);
         }
         return ans;
     }
+
+    // Submitted but avg. solution!
+    // int totalFruit(vector<int>& fruits){
+    //     unordered_map<int,int> map;
+
+    //     int ans=1;
+    //     int n=fruits.size();
+    //     int i=0, j=0;
+    //     while(i<n){
+    //         map[fruits[i]]++;
+    //         if(map.size() <= 2) ans = max(ans, i-j+1);
+    //         else{
+    //             if(map[fruits[j]] == 1) map.erase(fruits[j]);
+    //             else map[fruits[j]]--;
+    //             j++;
+    //         }
+    //         i++;
+    //     }
+    //     return ans;
+    // }
 };
