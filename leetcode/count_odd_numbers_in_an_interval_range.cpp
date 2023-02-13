74cac4e5a6de03e6f2935ff3150b8da0c7e7c9c2
IumoInfinium <iumoinfinium1331@gmail.com>
2023-02-13 22:31:35 +0530

    lc-u d-198 count odds in range with formula

--- leetcode/count_odd_numbers_in_an_interval_range.cpp
+++ leetcode/count_odd_numbers_in_an_interval_range.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int countOdds(int low, int high) {
-        int ans=0;
-        while(low<=high){
-            if(low==high && low&1){
-                ans++;
-                low++; high--;
-                continue;
-            }
-            if(low&1) ans++;
-            if(high&1) ans++;
-            low++; high--;
-        }
-        return ans;
+    // better memory, but not good runtime.
+    // int countOdds(int low, int high) {
+    //     int ans=0;
+    //     while(low<=high){
+    //         if(low==high && low&1){
+    //             ans++;
+    //             low++; high--;
+    //             continue;
+    //         }
+    //         if(low&1) ans++;
+    //         if(high&1) ans++;
+    //         low++; high--;
+    //     }
+    //     return ans;
+    // }
+    
+    // better runtime, but not good memory anymore.
+    int countOdds(int a, int b){
+        if(!(a&1) && !(b&1)) return (b-a)/2;
+        else return 1+(b-a)/2;
     }
 };
