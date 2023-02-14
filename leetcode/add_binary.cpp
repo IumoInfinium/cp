3170fce503eab3be0c09152f4015b56d3a88625c
IumoInfinium <iumoinfinium1331@gmail.com>
2023-02-14 12:08:19 +0530

    lc-u d-199 adding 2 bin strings, mem efficient

--- leetcode/add_binary.cpp
+++ leetcode/add_binary.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         if(a.length() > b.length()) return addBinary(b,a);
-        while(b.size() != a.size()) a = '0'+a;
+
+        while(b.length() != a.length()) a = '0'+a;
         int n = a.length();
-        int x,y,s,c = 0 ;
+
+        bool x,y,c=false;
         for(int i=n-1; i>=0; i--){
-           x = a[i]=='0' ? 0 : 1;
-           y = b[i]=='0' ? 0 : 1;
-           s = x^y^c;
-           a[i] = s? '1' : '0';
+           x = a[i]-'0';
+           y = b[i]-'0';
+           a[i] = (x^y^c) ? '1' : '0';
            c = (x&y) | c&(x^y);
         }
         if(c) a='1'+a;
