81d2d3ce5178b707394f405e47c3272e149c8b56
IumoInfinium <iumoinfinium1331@gmail.com>
2023-02-18 11:52:01 +0530

    lc d-203 classic bin tree inversion

---
+++ leetcode/invert_binary_tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return root;
+        invert(root);
+        return root;    
+    }
+
+    void invert(TreeNode* node){
+        if(!node) return;
+        TreeNode* left = node->left;
+        TreeNode* right = node->right;
+
+        node->left = right;
+        node->right = left;
+        invert(node->left);
+        invert(node->right);
+
+        return;
+    }
+};
