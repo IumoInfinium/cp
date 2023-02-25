4f2362eb9d3ddb20b60e414312d04a3499b11d5b
IumoInfinium <iumoinfinium1331@gmail.com>
2023-02-25 16:47:27 +0530

    lc d-210 kadane's algorithm based solution

--- leetcode/best_time_to_buy_and_sell_stock.cpp
+++ leetcode/best_time_to_buy_and_sell_stock.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int maxi=prices[n-1];
+        int n=prices.size(), maxi=prices[n-1];
         int ans=0;
         while(n--){
-            if(maxi < prices[n]) maxi=prices[n];
-            if(maxi > prices[n]) ans=max(ans,maxi-prices[n]);
+            if(maxi < prices[n]) maxi = prices[n];
+            if(maxi > prices[n]) ans = max(ans,maxi-prices[n]);
         }
         return ans;
     }
