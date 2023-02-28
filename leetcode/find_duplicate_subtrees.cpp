c61822a1c067840a29151ed4628e9e9e2e971f54
IumoInfinium <iumoinfinium1331@gmail.com>
2023-02-28 16:58:27 +0530

    lc-u d-213 find all duplicates in bin tree (less code, slightly inefficient)

--- leetcode/find_duplicate_subtrees.cpp
+++ leetcode/find_duplicate_subtrees.cpp
@@ -11,24 +11,43 @@
  */
 class Solution {
 public:
-    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-        // mapping every traversal and then finding only needed traversed subtrees from it
-        unordered_map< string, vector<TreeNode*>> map; 
-        vector<TreeNode*> ans;
+    // vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+    //     // mapping every traversal and then finding only needed traversed subtrees from it
+    //     unordered_map< string, vector<TreeNode*>> map; 
+    //     vector<TreeNode*> ans;
 
-        helper(root, map);
+    //     helper(root, map);
         
-        for(auto it = map.begin(); it!= map.end(); it++){
-            if(it->second.size() > 1)
-                ans.push_back(it->second[0]);
-        }
+    //     for(auto it = map.begin(); it!= map.end(); it++){
+    //         if(it->second.size() > 1)
+    //             ans.push_back(it->second[0]);
+    //     }
+    //     return ans;
+    // }
+
+    // string helper(TreeNode* node, unordered_map< string, vector<TreeNode*>>& map){
+    //     if(!node) return "#";
+    //     string s = "(" + helper(node->left, map) + to_string(node->val) + helper(node->right, map) + ")";
+    //     map[s].push_back(node);
+    //     return s;
+    // }
+
+    vector<TreeNode*> ans;
+    unordered_map<string,int> map;
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root){
+        string tmp = helper(root);
         return ans;
     }
 
-    string helper(TreeNode* node, unordered_map< string, vector<TreeNode*>>& map){
-        if(!node) return "#";
-        string s = "(" + helper(node->left, map) + to_string(node->val) + helper(node->right, map) + ")";
-        map[s].push_back(node);
-        return s;
+    string helper(TreeNode* node){
+        if(!node) return "";
+        string l = helper(node->left);
+        string r = helper(node->right);
+
+        string curr = to_string(node->val) + " " + l + " " + r;
+        if(map[curr] == 1) ans.push_back(node);
+        map[curr]++;
+
+        return curr;
     }
 };
