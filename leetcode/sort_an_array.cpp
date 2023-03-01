86f73aece5b021723bfaf3a159fc2a0131885a00
IumoInfinium <iumoinfinium1331@gmail.com>
2023-03-01 14:10:29 +0530

    lc d-214 sort an array - heap sort

---
+++ leetcode/sort_an_array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int it : nums) pq.push(it);
+        for(int i=0; i<nums.size(); i++){
+            nums[i] = pq.top();
+            pq.pop();
+        }
+        return nums;
+    }
+};
