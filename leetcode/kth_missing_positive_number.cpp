f9812b35b4f53b17d60654a5e75f443f8376e011
IumoInfinium <iumoinfinium1331@gmail.com>
2023-03-06 23:08:33 +0530

    lc-u d-219 k-th missing num in sorted array in O(logn)

--- leetcode/kth_missing_positive_number.cpp
+++ leetcode/kth_missing_positive_number.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
-    int findKthPositive(vector<int>& arr, int k) {
-        for(int i : arr) if(i<=k) k++;
-        return k;
+    // int findKthPositive(vector<int>& arr, int k) {
+    //     for(int i : arr) if(i<=k) k++;
+    //     return k;
+    // }
+     
+    // Even though, Binary Search, it is less efficient compared to above solution
+    int findKthPositive(vector<int>& arr, int k){
+        int m;
+        int l = 0, h = arr.size();
+        while(l<h){
+            m = (l+h)/2;
+            if(arr[m] - m - 1 < k) l=m+1;
+            else h=m;
+        }
+        return h+k;
     }
 };
