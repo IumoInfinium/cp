6742422aab267c923c783da69efa9a57943fb7e3
IumoInfinium <iumoinfinium1331@gmail.com>
2023-03-11 12:06:59 +0530

    lc-u d-224 convert a sorted arr to BST with (2-pointer approach)

--- leetcode/convert_sorted_list_to_binary_search_tree.cpp
+++ leetcode/convert_sorted_list_to_binary_search_tree.cpp
@@ -21,22 +21,43 @@
  */
 class Solution {
 public:
-    TreeNode* sortedListToBST(ListNode* head) {
-        vector<int> arr;
-        while(head!=NULL){
-            arr.push_back(head->val);
-            head = head->next;
-        }
-        TreeNode *root = makeTree(0,arr.size()-1, arr);
-        return root;
-    }
+    // TreeNode* sortedListToBST(ListNode* head) {
+    //     vector<int> arr;
+    //     while(head!=NULL){
+    //         arr.push_back(head->val);
+    //         head = head->next;
+    //     }
+    //     TreeNode *root = makeTree(0,arr.size()-1, arr);
+    //     return root;
+    // }
 
-    TreeNode* makeTree(int low, int high, vector<int>& arr){
-        if(low> high) return NULL;
-        int mid = (low+high)/2;
-        TreeNode* node = new TreeNode(arr[mid]);
-        node->left = makeTree(low, mid-1,arr);
-        node->right= makeTree(mid+1,high,arr);
+    // TreeNode* makeTree(int low, int high, vector<int>& arr){
+    //     if(low> high) return NULL;
+    //     int mid = (low+high)/2;
+    //     TreeNode* node = new TreeNode(arr[mid]);
+    //     node->left = makeTree(low, mid-1,arr);
+    //     node->right= makeTree(mid+1,high,arr);
+    //     return node;
+    // }
+
+    // Approach - 2
+    TreeNode *sortedListToBST(ListNode *head, ListNode *end=NULL){
+        if(!head) return NULL;
+        
+        ListNode *slow=head, *fast=head;
+        ListNode* prev=NULL;
+        while(slow && fast && fast->next){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        if(prev!=NULL) prev->next= NULL;
+
+        TreeNode *node = new TreeNode(slow->val);
+        if(slow == head) return node;
+        
+        node->left= sortedListToBST(head);
+        node->right = sortedListToBST(slow->next);
         return node;
     }
 };
