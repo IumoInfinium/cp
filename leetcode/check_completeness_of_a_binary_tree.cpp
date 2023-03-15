a715ba7e658019922f9259a6db7c59c921ea5177
IumoInfinium <iumoinfinium1331@gmail.com>
2023-03-15 16:12:27 +0530

    lc-u d-228 check if a tree is a complete binary tree (simplified)

--- leetcode/check_completeness_of_a_binary_tree.cpp
+++ leetcode/check_completeness_of_a_binary_tree.cpp
@@ -11,35 +11,52 @@
  */
 class Solution {
 public:
-    bool isCompleteTree(TreeNode* root) {
-        queue<TreeNode*> q;
-        q.push(root);
-        vector<int> values;
-        values.push_back(root->val);
-        while(!q.empty()){
-            int qsize = q.size();
-            while(qsize--){
-                TreeNode * curr = q.front();
-                q.pop();
+    // bool isCompleteTree(TreeNode* root) {
+    //     queue<TreeNode*> q;
+    //     q.push(root);
+    //     vector<int> values;
+    //     values.push_back(root->val);
+    //     while(!q.empty()){
+    //         int qsize = q.size();
+    //         while(qsize--){
+    //             TreeNode * curr = q.front();
+    //             q.pop();
 
-                if(curr->left){
-                    values.push_back(curr->left->val);
-                    q.push(curr->left);
-                }
-                else values.push_back(-1);
+    //             if(curr->left){
+    //                 values.push_back(curr->left->val);
+    //                 q.push(curr->left);
+    //             }
+    //             else values.push_back(-1);
 
-                if(curr->right){
-                    values.push_back(curr->right->val);
-                    q.push(curr->right);
-                }
-                else values.push_back(-1);
-            }
-        }
-        int last = -1;
-        for(int i=0;i<values.size();i++){
-            if(values[i]==-1) last = i;
-            if(last>-1 && values[i]>0) return false;
-        }
-        return true;
+    //             if(curr->right){
+    //                 values.push_back(curr->right->val);
+    //                 q.push(curr->right);
+    //             }
+    //             else values.push_back(-1);
+    //         }
+    //     }
+    //     int last = -1;
+    //     for(int i=0;i<values.size();i++){
+    //         if(values[i]==-1) last = i;
+    //         if(last>-1 && values[i]>0) return false;
+    //     }
+    //     return true;
+    // }
+
+    bool isCompleteTree(TreeNode* root){
+        int n = countNodes(root);
+        return check(root,0,n);
+    }
+    int countNodes(TreeNode* node){
+        if(!node) return 0;
+        return 1+ countNodes(node->left) + countNodes(node->right);
+    }
+
+    bool check(TreeNode* node,int pos, int n){
+        if(!node) return true;
+        if(pos >= n) return false;
+        bool l = check(node->left, pos*2+1,n);
+        bool r = check(node->right,pos*2+2,n);
+        return l && r;
     }
 };
