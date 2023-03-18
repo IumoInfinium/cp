f77bd281ee4c4c6689a55b8258f894819a774602
IumoInfinium <iumoinfinium1331@gmail.com>
2023-03-18 13:13:34 +0530

    lc-u d-231 doubly linked list based browser tab history

--- leetcode/design_browser_history.cpp
+++ leetcode/design_browser_history.cpp
@@ -1,35 +1,77 @@
+// class BrowserHistory {
+// public:
+//     stack<string> undoHistory, redoHistory;
+
+//     // this implementation works on always keeping the current website 
+//     // at top of the undoHistory stack;
+//     BrowserHistory(string homepage) {
+//         undoHistory.push(homepage);
+//         redoHistory= stack<string>();    
+//     }
+    
+//     void visit(string url) {
+//         undoHistory.push(url);
+//         redoHistory = stack<string>();
+//     }
+    
+//     string back(int steps) {
+//         while(steps && undoHistory.size() > 1){
+//             redoHistory.push(undoHistory.top());
+//             undoHistory.pop();
+//             steps--;
+//         }
+//         return undoHistory.top();
+//     }
+    
+//     string forward(int steps) {
+//         while(steps && redoHistory.size()){
+//             undoHistory.push(redoHistory.top());
+//             redoHistory.pop();
+//             steps--;
+//         }
+//         return undoHistory.top();
+//     }
+// };
+
+class Node{
+public:
+    string url;
+    Node* prev;
+    Node* next;
+    Node(string url){
+        this->url = url;
+        prev=NULL;
+        next=NULL;
+    }
+};
 class BrowserHistory {
 public:
-    stack<string> undoHistory, redoHistory;
-
-    // this implementation works on always keeping the current website 
-    // at top of the undoHistory stack;
+    Node* curr;
     BrowserHistory(string homepage) {
-        undoHistory.push(homepage);
-        redoHistory= stack<string>();    
+        curr = new Node(homepage);
     }
     
     void visit(string url) {
-        undoHistory.push(url);
-        redoHistory = stack<string>();
+        Node* tmp = new Node(url);
+        tmp->prev = curr;
+        curr->next = tmp;
+        curr = tmp; 
     }
     
     string back(int steps) {
-        while(steps && undoHistory.size() > 1){
-            redoHistory.push(undoHistory.top());
-            undoHistory.pop();
-            steps--;
+        while(steps--){
+            if(!curr->prev) return curr->url;
+            curr = curr->prev;
         }
-        return undoHistory.top();
+        return curr->url;
     }
     
     string forward(int steps) {
-        while(steps && redoHistory.size()){
-            undoHistory.push(redoHistory.top());
-            redoHistory.pop();
-            steps--;
+        while(steps--){
+            if(!curr->next) return curr->url;
+            curr = curr->next;
         }
-        return undoHistory.top();
+        return curr->url;
     }
 };
 
