146ec7b8d6b4ee02f340782f5359d1cb8196a158
IumoInfinium <iumoinfinium1331@gmail.com>
2023-04-02 19:12:51 +0530

    lc-u d-246 using upper_bound to get min val

--- leetcode/successful_pairs_of_spells_and_potions.cpp
+++ leetcode/successful_pairs_of_spells_and_potions.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        int n = spells.size();
-        int m = potions.size();
+    // vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+    //     int n = spells.size();
+    //     int m = potions.size();
+    //     sort(begin(potions), end(potions));
+
+    //     for(int i=0; i<n; i++){
+    //         int low = 0, high = m-1;
+    //         int mid;
+    //         while(low <= high){
+    //             mid = low + (high-low)/2;
+    //             if(spells[i] >= success &&  potions[mid] >1) high=mid-1;
+    //             else if(potions[mid] >= success && spells[i]>1) high=mid-1;
+    //             else if((long)spells[i]* (long)potions[mid] >= success) high=mid-1;
+    //             else low = mid+1;
+    //         }
+    //         spells[i] = m-low;
+    //     }
+    //     return spells;
+    // }
+
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success){
+        int n=spells.size(), m = potions.size();
+
         sort(begin(potions), end(potions));
 
-        vector<int> ans(n,0);
         for(int i=0; i<n; i++){
-            int low = 0, high = m-1;
-            int mid;
-            while(low <= high){
-                mid = low + (high-low)/2;
-                if(spells[i] >= success &&  potions[mid] >1) high=mid-1;
-                else if(potions[mid] >= success && spells[i]>1) high=mid-1;
-                else if((long)spells[i]* (long)potions[mid] >= success) high=mid-1;
-                else low = mid+1;
-            }
-            ans[i] = m-low;
+            spells[i] = potions.end() - upper_bound(begin(potions), end(potions), ((success-1 )/ spells[i]));
         }
-        return ans;
+        return spells;
     }
 };
