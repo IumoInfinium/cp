2e001efce9b2213955f6a274fee3c628a50aec57
IumoInfinium <iumoinfinium1331@gmail.com>
2023-04-04 11:08:43 +0530

    lc-u d-248 better memory usage for mapping

--- leetcode/optimal_partition_of_string.cpp
+++ leetcode/optimal_partition_of_string.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int partitionString(string s) {
-        unordered_map<char,bool> mp;
+        bool mp[26]={0};
         int n = s.size();
         int ans=1;
         for(int i=0; i<n; i++){
-            if(mp.count(s[i])){
-                mp.clear();
-                mp[s[i]] = true;
+            if(mp[s[i]-'a']){
+                memset(mp,false, sizeof(mp));
+                mp[s[i]-'a'] = true;
                 ans++;
             }
             else{
-                mp[s[i]]= true;
+                mp[s[i]-'a']= true;
             }
         }
         return ans;
