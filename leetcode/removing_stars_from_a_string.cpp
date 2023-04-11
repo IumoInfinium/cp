9676b71ba4e3e5a7ce7f0b318480822067ebfe2a
IumoInfinium <iumoinfinium1331@gmail.com>
2023-04-11 21:46:38 +0530

    lc d-255 string operations with stack

---
+++ leetcode/removing_stars_from_a_string.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    // string removeStars(string s) {
+    //     int n=s.size();
+    //     unordered_set<int> pos;
+    //     for(int i=n-1; i>=0; i--){
+    //         if(s[i]!= '*' || pos.count(i)) continue;
+    //         if(s[i] == '*'){
+    //             int j= i-1;
+    //             for(; j>=0; j--){
+    //                 if(pos.count(j)) continue;
+    //                 if(s[j] !='*' && !pos.count(j)) break;
+    //             }
+    //             pos.insert(i);
+    //             pos.insert(j);
+    //         }
+    //     }
+    //     string ans="";
+    //     for(int i=0;i<n;i++)
+    //         if(!pos.count(i)) ans+=s[i];
+    //     return ans;
+    // }
+    
+    string removeStars(string s){
+        int n = s.size();
+        stack<char> st;
+        int i=0;
+        while(i<n){
+            if(s[i] != '*') st.push(s[i++]);
+            else{
+                st.pop();
+                i++;
+            }
+        }
+        string ans="";
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(begin(ans),end(ans));
+        return ans;
+    }
+};
