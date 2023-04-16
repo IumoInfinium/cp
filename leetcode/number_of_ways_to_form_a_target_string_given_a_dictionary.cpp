18cd64ec62be166c8982b00bb61043e53087fbb9
IumoInfinium <iumoinfinium1331@gmail.com>
2023-04-16 16:47:36 +0530

    lc d-260 count ways of forming a string from given strings

---
+++ leetcode/number_of_ways_to_form_a_target_string_given_a_dictionary.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int m;
+    int k;
+    int memo[1001][1001];
+    const int  MOD = 1e9+7;
+    int numWays(vector<string>& words, string target) {
+        m = target.size();
+        k = words[0].size();
+        vector<vector<long long>> freq(26, vector<long long>(k,0));
+
+        for(int j=0; j<k; j++){
+            for(string & w: words) freq[w[j]-'a'][j]++;
+        }
+
+        memset(memo, -1, sizeof(memo));
+        return solve(0,0, freq, target);
+    }
+    int solve(int i, int j, vector<vector<long long>>& freq, string& target){
+        if(i == m) return 1;
+        if(j == k) return 0;
+        if(memo[i][j] != -1) return memo[i][j];
+        int not_take = solve(i,j+1, freq, target) % MOD ;
+        int take = ( freq[target[i]-'a'][j] * solve(i+1, j+1, freq, target)) % MOD;
+
+        return memo[i][j] = (take + not_take)% MOD;
+    }
+};
