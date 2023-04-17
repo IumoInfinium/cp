31d7d413b4dc4ab2ad9c6270afbf76da79f26842
IumoInfinium <iumoinfinium1331@gmail.com>
2023-04-17 22:28:16 +0530

    lc d-261 return bool arr if ith is big enough

---
+++ leetcode/kids_with_the_greatest_number_of_candies.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxi=0;
+        for(int i: candies) maxi = max(maxi,i);
+        
+        vector<bool> ans(candies.size(),false);
+        for(int i=0; i<candies.size(); i++){
+            if(candies[i] + extraCandies >= maxi) ans[i] = true;
+        }
+        return ans;
+    }
+};
