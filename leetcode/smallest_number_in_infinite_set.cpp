7142956445fe389feeff27b9fd9108f340b00278
IumoInfinium <iumoinfinium1331@gmail.com>
2023-04-25 19:39:18 +0530

    lc d-269 sett based approach to track element

---
+++ leetcode/smallest_number_in_infinite_set.cpp
@@ -0,0 +1,28 @@
+class SmallestInfiniteSet {
+public:
+    set<int> sett;
+    int small;
+    SmallestInfiniteSet() {
+        small=1;
+    }
+    
+    int popSmallest() {
+        if(!sett.empty()){
+            int x = *sett.begin();
+            sett.erase(sett.begin());
+            return x;
+        }
+        return small++;
+    }
+    
+    void addBack(int num) {
+        if(num < small) sett.insert(num);
+    }
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
