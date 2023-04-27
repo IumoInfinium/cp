a4cb6afbf17b62695fb17f04afaaeaa14aad7bd5
IumoInfinium <iumoinfinium1331@gmail.com>
2023-04-27 20:23:44 +0530

    lc-u d-271 finding possible sqr roots in with bin. search

--- leetcode/bulb_switcher.cpp
+++ leetcode/bulb_switcher.cpp
@@ -1,6 +1,27 @@
+// class Solution {
+// public:
+//     int bulbSwitch(int n) {
+//         return sqrt(n);
+//     }
+// };
+
 class Solution {
 public:
     int bulbSwitch(int n) {
-        return sqrt(n);
+        int l=1, h=n;
+        int ans=0;
+
+        while(l<=h){
+            long mid = l +(h-l)/2;
+            long sqr = mid*mid;
+
+            if(sqr == n) return (int)mid;
+            else if(sqr < n){
+                l = mid+1;
+                ans = mid;
+            }
+            else h=mid-1;
+        }
+        return (int)ans;
     }
 };
