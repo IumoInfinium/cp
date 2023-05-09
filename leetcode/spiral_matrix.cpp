d38c5dfac041c97e9a80e67b91b8fd6dafe1ad79
IumoInfinium <iumoinfinium1331@gmail.com>
2023-05-09 23:49:14 +0530

    lc d-283 sequencial spiral matrix print

--- leetcode/spiral_matrix.cpp
+++ leetcode/spiral_matrix.cpp
@@ -25,3 +25,40 @@ public:
         return false;
     }
 };
+
+class Solution2 {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> ans;
+        
+        int top=0, right=n-1;
+        int bottom=m-1, left=0;
+
+        while(top <= bottom && left <= right){
+            for(int i=left; i<= right && top<= bottom ; i++){
+                ans.emplace_back(matrix[top][i]);
+            }
+            top++;
+
+            for(int i=top; i<=bottom && left<=right; i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+
+            
+            for(int i=right; i>=left && top<=bottom ; i--){
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+            
+            for(int i=bottom; i>=top && left<=right; i--){
+                ans.emplace_back(matrix[i][left]);
+            }
+            left++;
+        }
+        return ans;
+    }
+};
