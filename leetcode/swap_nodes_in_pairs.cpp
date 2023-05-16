f2bd512f87421831401e4a17ec4f6939fc056b21
IumoInfinium <iumoinfinium1331@gmail.com>
2023-05-16 15:33:45 +0530

    lc d-290 swapping adjacent nodes in linked list

--- leetcode/swap_nodes_in_pairs.cpp
+++ leetcode/swap_nodes_in_pairs.cpp
@@ -8,7 +8,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution1 {
 public:
     ListNode* swapPairs(ListNode* head) {
         if(!head || !head->next) return head;
@@ -23,6 +23,38 @@ public:
             curr->next=tmp;
             head=head->next;
             curr=curr->next->next;
+        }
+        return dummy->next;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next) return head;
+
+        ListNode *dummy = new ListNode();
+
+        ListNode *prev = dummy;
+        ListNode *curr = head;
+
+        while(curr && curr->next){
+            prev->next = curr->next;
+            curr->next = prev->next->next;
+            prev->next->next = curr;
+
+            prev = curr;
+            curr = curr->next;
         }
         return dummy->next;
     }
