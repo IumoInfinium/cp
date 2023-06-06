a2129b842c2950f12e6e9dea2a442500d46711ef
IumoInfinium <iumoinfinium1331@gmail.com>
2023-06-06 14:46:43 +0530

    lc d-311 check if arr has same diff

---
+++ leetcode/can_make_arithmetic_progression_from_sequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        int n = arr.size();
+        if(n == 2) return true;
+        
+        sort(arr.begin(), arr.end());
+        int last = arr[1]-arr[0];
+        
+        for(int i=2; i< arr.size(); i++){
+            if(arr[i]-arr[i-1] != last) return false;
+        }
+        return true;
+    }
+};
